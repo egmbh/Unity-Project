29625f6c4f853d3b7831595c2686b3eb3fb84df3
123654-dev <72887184+123654-dev@users.noreply.github.com>
2023-11-10 12:34:12 +0100

    Update: Zwischenstand 10.11.2023
    Funktionalität für den Meteoriten-Spawner (Spawner.cs), Position wird zufällig ermittelt

---
+++ Scripts/Komet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Komet : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x, pos.y - speed * Time.deltaTime, pos.z);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject player = collision.gameObject;
+        player.GetComponent<Player>().Die();
+    }
+}
