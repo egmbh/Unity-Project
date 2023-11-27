00aa795db7faf547b6327e7b3fe4fe7591d6e0cc
123654-dev <72887184+123654-dev@users.noreply.github.com>
2023-11-27 12:08:43 +0100

    Explosionen + Sprites

--- Assets/Scripts/Komet.cs
+++ Assets/Scripts/Komet.cs
@@ -1,36 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class Komet : MonoBehaviour
 {
-    public float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject explosion;
 
-    // Update is called once per frame
+    private float speed = 0;
+
+    //public GameObject explosionPrefab;
+
+    // Update is called once per frame (Methode von Unity)
     void Update()
     {
-        Vector3 pos = transform.position;
-        transform.position = new Vector3(pos.x, pos.y - speed * Time.deltaTime, pos.z);
+        transform.position += new Vector3(0, -1 * speed * Time.deltaTime, 0);
     }
 
+    // Set the speed of the meteor
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    // Zerstört den Meteor, wenn er aus dem Bildschirm fliegt (Methode von Unity)
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Wird aufgerufen, wenn ein Objekt mit einem Collider, der auf "Trigger" gesetzt ist,
+    // also andere Objekte nur registriert, aber trotzdem durch sie durchgehen kann, auf ein Objekt stößt (Methode von Unity)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        GameObject other = collision.gameObject;
-        if(other.name == "LePlayeur")
+        // Man kann Objekten eigene Tags zuweisen, an denen sie erkannt werden können
+        if (col.gameObject.CompareTag("Bullet"))
         {
-            other.GetComponent<Player>().Die();
+            // Wenn es sich um eine Kugel handelt, werden sie und der Meteor zerstört und ein Explosions-Objekt wird erzeugt
+            //Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(col.gameObject);
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.CompareTag("Player"))
+        {
+            // Wenn es hingegen ein Spieler ist, wird die Methode "Die" im Spieler aufgerufen
+            col.gameObject.GetComponent<Player>().Die();
         }
     }
 }
